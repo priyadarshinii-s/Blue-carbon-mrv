6293ffb01f11740e48a6121d2d3602536381ffde
Arulselvan-65 <arulselvan2244@gmail.com>
2026-03-12 13:01:04 +0530

    upated

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "https://blue-carbon-mrv-zs76.onrender.com";
+const API_BASE = "https://blue-carbon-mrv-zs76.onrender.com/api";
 const api = axios.create({
     baseURL: API_BASE,
     headers: { "Content-Type": "application/json" },
